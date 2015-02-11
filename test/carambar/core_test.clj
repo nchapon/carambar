6cffc2fc14b608728bc884d04960c2570df4fb87
Nicolas Chapon <nchapon@gmail.com>
2015-02-11 23:25:54 +0100

    Playing with zippers

--- test/carambar/core_test.clj
+++ test/carambar/core_test.clj
@@ -1,12 +1,39 @@
 (ns carambar.core-test
-  (:require [clojure.test :refer :all]
-            [carambar.core :refer :all]))
+  (:require [carambar.core :refer :all]
+            [midje.sweet :refer :all]
+            [clojure.zip :as zip]
+            [clojure.xml :as xml]
+            [clojure.data.zip.xml :as zip-xml]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def data "<project>
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.carambar</groupId>
+  <artifactId>simple</artifactId>
+  <version>1.0-SNAPSHOT</version>
+  <packaging>jar</packaging>
+  <name>simple</name>
+  <url>http://maven.apache.org</url>
+  <properties>
+    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+  </properties>
+  <dependencies>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>4.11</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+</project>")
 
 
-(comment
-  (require '[clojure.xml :as xml])
-  (xml/parse "test/pom.xml"))
+(def xml-tree
+  (-> data
+    .getBytes
+    java.io.ByteArrayInputStream.
+    xml/parse ))
+
+(def pom (zip/xml-zip xml-tree))
+
+(fact (zip-xml/xml1-> pom :name zip-xml/text) => "simple")
