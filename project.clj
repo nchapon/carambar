2eff9d0d864daff718dcdad8198641292df6646b
Nicolas Chapon <nchapon@gmail.com>
2015-02-25 22:29:02 +0100

    Initialize ring / compojure / json application

--- project.clj
+++ project.clj
@@ -4,5 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.zip "0.1.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+                 [org.clojure/data.zip "0.1.1"]
+                 [ring/ring-core "1.3.2"]
+                 [compojure "1.3.2"]
+                 [ring/ring-json "0.3.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :plugins [[lein-ring "0.9.2"]]
+  :uberjar-name "carambar.jar"
+  :ring {:handler carambar.core/app
+         :init carambar.core/init})
--- src/carambar/core.clj
+++ src/carambar/core.clj
@@ -1,6 +1,17 @@
-(ns carambar.core)
+(ns carambar.core
+  (:use ring.util.response)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.core :refer [GET defroutes]]
+            [ring.middleware.json :as middleware]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+(defroutes app-routes
+  (GET "/" [] (response {:carambar "OK"}))
+  (GET "/packages" [q] (response [{:package "org.apache.logging"}
+                                 {:package "com.google.guava"}])))
+
+(def app (-> (handler/api app-routes)
+             (middleware/wrap-json-body)
+             (middleware/wrap-json-response)))
