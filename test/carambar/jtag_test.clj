6741629fe5ba2fa8360ea472f34ddc5f19d0e4c2
Nicolas Chapon <nchapon@gmail.com>
2015-03-08 23:29:43 +0100

    WIP read zip content file

--- test/carambar/jtag_test.clj
+++ test/carambar/jtag_test.clj
@@ -5,19 +5,42 @@
 
 ;; /home/nchapon/opt/m2_repo/org/slf4j/slf4j-api/1.7.6/slf4j-api-1.7.6-sources.jar
 
-(def filename "/home/nchapon/opt/m2_repo/org/slf4j/slf4j-api/1.7.6/slf4j-api-1.7.6-sources.jar")
+(def filename "/home/nchapon/opt/m2_repo/org/slf4j/slf4j-api/1.7.2/slf4j-api-1.7.2-sources.jar")
 
 
+(defn java-file?
+  "Is java file"
+  [f]
+  (re-matches #".*\.java$" f))
+
+(defn entries
+  "Zipfile entries"
+  [zipfile]
+  (enumeration-seq (.entries zipfile)))
+
+
+(defn read-content
+  "read content"
+  [entry zipfile]
+  (when (java-file? (.getName entry))
+    (with-open [rdr (clojure.java.io/reader
+                     (java.io.InputStreamReader. (.getInputStream zipfile entry)))]
+      (reduce conj [] (line-seq rdr)))))
+
+
+(defn filter-package
+  "doc-string"
+  [lines]
+  (filter #(re-matches #"^package (.*);$" %) lines))
 
 (defn filenames-in-zip
   "doc-string"
   [f]
   (let [z (java.util.zip.ZipFile. f)]
-    (map #(.getName %) (enumeration-seq (.entries z)))))
+    (map #(filter-package (read-content % z)) (entries z))))
 
 
 (filenames-in-zip filename)
-
 
 
 (def lines (clojure.string/split-lines (slurp "test/App.java")))
