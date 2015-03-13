39144e6f007ee8c1880e80d4f9b6e601e35e5490
Nicolas Chapon <nchapon@gmail.com>
2015-03-13 23:42:12 +0100

    Create jarfile before tests

--- test/carambar/cache_test.clj
+++ test/carambar/cache_test.clj
@@ -5,29 +5,18 @@
   (:import [java.util.zip ZipEntry ZipOutputStream]))
 
 
-(fact (add-entry {:name "/path/test.jar" :values []}) => (contains {:name "/path/test.jar" :values []}))
+(fact "Add entry"
+  (add-entry {:name "/path/test.jar" :values []}) => (contains {:name "/path/test.jar" :values []}))
+
+(fact "Convert filename to javaclas."
+  (filename->javaclass "a/b/c/MyClass.class") => "a.b.c.MyClass")
 
 ;; need to run mvn clean install before...
 (def jarfile-with-two-classes "/home/nchapon/opt/m2_repo/org/carambar/simple/1.0-SNAPSHOT/simple-1.0-SNAPSHOT.jar")
 
-(def slf4j "/home/nchapon/opt/m2_repo/org/slf4j/slf4j-api/1.7.8/slf4j-api-1.7.8.jar")
-
 (fact (:values (parse jarfile-with-two-classes)) => (contains ["org.carambar.App" "org.carambar.MyClass"] :in-any-order))
 
-(fact (filename->javaclass "a/b/c/MyClass.class") => "a.b.c.MyClass")
-
-
 (def tmpdir (System/getProperty "java.io.tmpdir"))
-
-
-
-
-;;(.mkdir (java.io.File. (str tmpdir "/carambar")))
-
-;;(map #(.mkdir (java.io.File. (str  tmpdir "/carambar/" %))) ["a" "a/b" "a/b/c"])
-
-;;(map  #(spit (str tmpdir "/carambar/a/b/c/" %) "CONTENT") ["A.class"])
-
 
 (defmacro ^:private with-entry
   [zip entry-name & body]
@@ -37,25 +26,16 @@
      (flush)
      (.closeEntry zip#)))
 
-(defn jarfile [filename]
-  (with-open [file (io/output-stream (str tmpdir "/" filename))
+(defn jarfile [filename classes]
+  (with-open [file (io/output-stream filename)
               zip  (ZipOutputStream. file)
               wrt  (io/writer zip)]
     (binding [*out* wrt]
-      (doto zip
-        (with-entry "Foo.class"
-          (println "foo"))
-        (with-entry "bar/Baz.class"
-          (println "baz"))))))
+      (dotimes [i (count classes)]
+        (with-entry zip (nth classes i)
+              (println "foo"))))))
 
-
-;; (with-open [output (ZipOutputStream. (io/output-stream "foo.zip"))
-;;             input  (io/input-stream "foo")]
-;;   (with-entry output "foo"
-;;     (io/copy input output)))
-
-
-(jarfile "test.jar")
-
-
-(parse (str tmpdir "/" "test.jar"))
+(fact "Parse one jar with two classes"
+  (let [filename (str tmpdir "/" "test.jar")
+        jar (jarfile filename ["Foo.class" "bar/Baz.class"])]
+    (parse filename) => {:name "/tmp/test.jar" :values ["Foo" "bar.Baz"]}))
