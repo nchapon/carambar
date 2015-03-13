af7402f116ee85aba7195a4a1d442106e9e8ebc1
Nicolas Chapon <nchapon@gmail.com>
2015-03-13 17:34:45 +0100

    Create dummy jar for tests

--- test/carambar/cache_test.clj
+++ test/carambar/cache_test.clj
@@ -1,6 +1,8 @@
 (ns carambar.cache-test
   (:require [midje.sweet :refer :all]
-            [carambar.cache :refer :all]))
+            [carambar.cache :refer :all]
+            [clojure.java.io :as io])
+  (:import [java.util.zip ZipEntry ZipOutputStream]))
 
 
 (fact (add-entry {:name "/path/test.jar" :values []}) => (contains {:name "/path/test.jar" :values []}))
@@ -13,3 +15,47 @@
 (fact (:values (parse jarfile-with-two-classes)) => (contains ["org.carambar.App" "org.carambar.MyClass"] :in-any-order))
 
 (fact (filename->javaclass "a/b/c/MyClass.class") => "a.b.c.MyClass")
+
+
+(def tmpdir (System/getProperty "java.io.tmpdir"))
+
+
+
+
+;;(.mkdir (java.io.File. (str tmpdir "/carambar")))
+
+;;(map #(.mkdir (java.io.File. (str  tmpdir "/carambar/" %))) ["a" "a/b" "a/b/c"])
+
+;;(map  #(spit (str tmpdir "/carambar/a/b/c/" %) "CONTENT") ["A.class"])
+
+
+(defmacro ^:private with-entry
+  [zip entry-name & body]
+  `(let [^ZipOutputStream zip# ~zip]
+     (.putNextEntry zip# (ZipEntry. ~entry-name))
+     ~@body
+     (flush)
+     (.closeEntry zip#)))
+
+(defn jarfile [filename]
+  (with-open [file (io/output-stream (str tmpdir "/" filename))
+              zip  (ZipOutputStream. file)
+              wrt  (io/writer zip)]
+    (binding [*out* wrt]
+      (doto zip
+        (with-entry "Foo.class"
+          (println "foo"))
+        (with-entry "bar/Baz.class"
+          (println "baz"))))))
+
+
+;; (with-open [output (ZipOutputStream. (io/output-stream "foo.zip"))
+;;             input  (io/input-stream "foo")]
+;;   (with-entry output "foo"
+;;     (io/copy input output)))
+
+
+(jarfile "test.jar")
+
+
+(parse (str tmpdir "/" "test.jar"))
