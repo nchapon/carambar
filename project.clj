52cb2d1a7cb2bef57b9175662c15d66637c6adfa
Nicolas Chapon <nchapon@gmail.com>
2015-04-14 13:51:01 +0200

    Use clojure.tools.logging with logback.

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.12"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [ring/ring-core "1.3.2"]
                  [compojure "1.3.2"]
                  [ring/ring-json "0.3.1"]]
--- src/carambar/core.clj
+++ src/carambar/core.clj
@@ -4,12 +4,19 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [compojure.core :refer [GET defroutes]]
-            [ring.middleware.json :as middleware]))
+            [ring.middleware.json :as middleware]
+            [clojure.tools.logging :as log]))
 
 (defroutes app-routes
   (GET "/" [] (response {:carambar "OK"}))
   (GET "/index" [] (response (repository/create-repo)))
   (GET "/classes" [search] (response {:classes (repository/find-class search)})))
+
+(defn init
+  "Init carambar settings"
+  []
+  (log/info "Init carambar"))
+
 
 (def app (-> (handler/api app-routes)
              (middleware/wrap-json-body)
