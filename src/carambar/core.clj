a680fdb400641f7fa5ecd8d38703fee593b637c2
Nicolas Chapon <nchapon@gmail.com>
2015-04-23 16:41:00 +0200

    Add project with POST method

--- src/carambar/core.clj
+++ src/carambar/core.clj
@@ -4,13 +4,15 @@
             [carambar.mvn :as mvn]
             [compojure.handler :as handler]
             [compojure.route :as route]
-            [compojure.core :refer [GET defroutes]]
+            [compojure.core :refer [GET POST defroutes]]
             [ring.middleware.json :as middleware]
             [clojure.tools.logging :as log]))
 
 (defroutes app-routes
   (GET "/" [] (response {:carambar "OK"}))
-  (GET "/projects" [] (response (repository/add-project "test_projects/simple")))
+  (POST "/projects" req
+        (let [path (get-in req [:body :path])]
+          (response (repository/add-project path))))
   (GET "/projects/:name/classes" [name search] (response {:classes (repository/find-class name search)})))
 
 (defn init
@@ -20,5 +22,5 @@
 
 
 (def app (-> (handler/api app-routes)
-             (middleware/wrap-json-body)
+             (middleware/wrap-json-body {:keywords? true})
              (middleware/wrap-json-response)))
